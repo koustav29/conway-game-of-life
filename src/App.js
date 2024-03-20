7899dd887e3c45fb42673af52bdb05d753a461e2
koustav29 <koustavkhanda7@gmail.com>
2024-03-20 23:45:27 +0530

    Conway Game of Life

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ const numCols = 50;
 
 const generateNewGrid = () => {
   const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
   return rows;
 };
 function App() {
   const operations = [
-    [0,1],
-    [0,-1],
-    [1,-1],
-    [-1,1],
-    [1,1],
-    [-1,-1],
-    [1,0],
-    [-1,0]
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0]
   ];
   //initializing grid
   const [grid, setGrid] = useState(() => {
@@ -37,33 +37,35 @@ function App() {
   runningRef.current = running;
 
   //using callback function for generating the function only once but the state of the grid will change and as will change the grid accordingly
-  const runSimulation = useCallback(()=>{
-    if(!runningRef.current) return;
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) return;
     //simulate 
-    setGrid(g=>{
-      return produce(g,gridCopy => {
-        for(let i=0;i<numRows;i++){
-          for(let j=0;j<numCols;j++){
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
             let neighbours = 0;
-            operations.forEach(([x,y])=>{
-              const newI = i+x;
-              const newJ = j+y;
-              if(newI>=0 && newI<numRows && newJ>=0 && newJ<numCols) neighbours += g[newI][newJ];
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) neighbours += g[newI][newJ];
             })
-            if(neighbours< 2 ||neighbours>3) gridCopy[i][j]=0;
-            else if(g[i][j]===0 && neighbours===3) gridCopy[i][j]=1;
+            if (neighbours < 2 || neighbours > 3) gridCopy[i][j] = 0;
+            else if (g[i][j] === 0 && neighbours === 3) gridCopy[i][j] = 1;
           }
         }
       })
     });
-    setTimeout(runSimulation,1000);
-  },[]);
+    setTimeout(runSimulation, 1000);
+  }, []);
   return (
-    <>
-      <button onClick={()=>{
+    <div style={{
+      alignItems:'center'
+    }}>
+      <button onClick={() => {
         setRunning(!running);
-        if(!running){
-          runningRef.current=true;
+        if (!running) {
+          runningRef.current = true;
           runSimulation();
         }
       }}>
@@ -71,16 +73,19 @@ function App() {
       </button>
 
       {/* clear matrix */}
-      <button onClick={()=>{
+      <button onClick={() => {
         setGrid(generateNewGrid());
       }}>clear</button>
-      <button onClick={()=>{
+      <button onClick={() => {
         const rows = [];
         for (let i = 0; i < numRows; i++) {
-          rows.push(Array.from(Array(numCols), () => Math.random()>0.5?1:0));
+          rows.push(Array.from(Array(numCols), () => Math.random() > 0.5 ? 1 : 0));
         }
         setGrid(rows);
       }}>random</button>
+
+
+      {/* body */}
       <div style={{
         display: 'grid',
         gridTemplateColumns: `repeat(${numCols},20px)`
@@ -99,14 +104,13 @@ function App() {
                 width: 20,
                 height: 20,
                 backgroundColor: grid[i][j] ? "#5EFF00" : undefined,
-                border: "solid 1px black",
-                borderRadius: '2px'
+                border: "solid 1px black"
               }}
             />
           ))
         )}
       </div>
-    </>
+    </div>
   );
 }
 
